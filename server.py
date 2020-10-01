a25d8b18f1420a9846f97ffdb22ee840dd12af36
xFluke <mikoman1124@gmail.com>
2020-10-01 19:17:03 -0400

    Completed Lab2

--- server.py
+++ server.py
@@ -11,60 +11,76 @@ connected = 0
 
 clients = {}
 
-def connectionLoop(sock):
-   while True:
-      data, addr = sock.recvfrom(1024)
-      data = str(data)
-      if addr in clients:
-         if 'heartbeat' in data:
-            clients[addr]['lastBeat'] = datetime.now()
-      else:
-         if 'connect' in data:
-            clients[addr] = {}
-            clients[addr]['lastBeat'] = datetime.now()
-            clients[addr]['color'] = 0
-            message = {"cmd": 0,"player":{"id":str(addr)}}
-            m = json.dumps(message)
-            for c in clients:
-               sock.sendto(bytes(m,'utf8'), (c[0],c[1]))
 
-def cleanClients():
-   while True:
-      for c in list(clients.keys()):
-         if (datetime.now() - clients[c]['lastBeat']).total_seconds() > 5:
-            print('Dropped Client: ', c)
-            clients_lock.acquire()
-            del clients[c]
-            clients_lock.release()
-      time.sleep(1)
+def connectionLoop(sock):
+    while True:
+        data, addr = sock.recvfrom(1024)
+        data = str(data)
+        if addr in clients:
+            if 'heartbeat' in data:
+                clients[addr]['lastBeat'] = datetime.now()
+        else:
+            if 'connect' in data:
+                clients[addr] = {}
+                clients[addr]['lastBeat'] = datetime.now()
+                clients[addr]['color'] = 0
+                message = {"cmd": 0, "player": {"id": str(addr)}}
+                m = json.dumps(message)
+                for c in clients:
+                    sock.sendto(bytes(m, 'utf8'), (c[0], c[1]))
+
+                    message = {"player": {"id": str(addr)}}
+                    m = json.dumps(message)
+                    sock.sendto(bytes(m, 'utf8'), (addr[0], addr[1]))
+
+                print(addr[0])
+                print(addr[1])
+
+def cleanClients(sock):
+    while True:
+        for c in list(clients.keys()):
+            if (datetime.now() - clients[c]['lastBeat']).total_seconds() > 5:
+                print('Dropped Client: ', c)
+                message = {"cmd": 2, "player": {"id": str(c)}}
+                m = json.dumps(message)
+                for cl in list(clients.keys()):
+                    sock.sendto(bytes(m, 'utf8'), (cl[0], cl[1]))
+                clients_lock.acquire()
+                del clients[c]
+                clients_lock.release()
+        time.sleep(1)
+
 
 def gameLoop(sock):
-   while True:
-      GameState = {"cmd": 1, "players": []}
-      clients_lock.acquire()
-      print (clients)
-      for c in clients:
-         player = {}
-         clients[c]['color'] = {"R": random.random(), "G": random.random(), "B": random.random()}
-         player['id'] = str(c)
-         player['color'] = clients[c]['color']
-         GameState['players'].append(player)
-      s=json.dumps(GameState)
-      print(s)
-      for c in clients:
-         sock.sendto(bytes(s,'utf8'), (c[0],c[1]))
-      clients_lock.release()
-      time.sleep(1)
+    while True:
+        GameState = {"cmd": 1, "players": []}
+        clients_lock.acquire()
+        print(clients)
+        for c in clients:
+            player = {}
+            clients[c]['color'] = {"R": random.random(
+            ), "G": random.random(), "B": random.random()}
+            player['id'] = str(c)
+            player['color'] = clients[c]['color']
+            GameState['players'].append(player)
+        s = json.dumps(GameState)
+        print(s)
+        for c in clients:
+            sock.sendto(bytes(s, 'utf8'), (c[0], c[1]))
+        clients_lock.release()
+        time.sleep(1)
+
 
 def main():
-   port = 12345
-   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-   s.bind(('', port))
-   start_new_thread(gameLoop, (s,))
-   start_new_thread(connectionLoop, (s,))
-   start_new_thread(cleanClients,())
-   while True:
-      time.sleep(1)
+    port = 12345
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.bind(('', port))
+    start_new_thread(gameLoop, (s,))
+    start_new_thread(connectionLoop, (s,))
+    start_new_thread(cleanClients, (s,))
+    while True:
+        time.sleep(1)
+
 
 if __name__ == '__main__':
-   main()
+    main()
